fecbabd1a4146f2d75e0030d4b22edab5e55b092
omrobbie <putu.roby@yahoo.com>
2018-08-28 00:00:04 +0700

    create dummy rebound list

---
+++ ex/Rebound/ReboundListTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReboundListTableViewCell.swift
+//  ex
+//
+//  Created by omrobbie on 27/08/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ReboundListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgUser: UIImageView!
+    @IBOutlet weak var txtFullName: UILabel!
+    @IBOutlet weak var txtLastChat: UILabel!
+    
+}
---
+++ ex/Rebound/ReboundListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ReboundListViewController.swift
+//  ex
+//
+//  Created by omrobbie on 27/08/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ReboundListViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var tableViewReboundList: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableViewReboundList.delegate = self
+        self.tableViewReboundList.dataSource = self
+    }
+}
+
+extension ReboundListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewReboundList.dequeueReusableCell(withIdentifier: "cellReboundList", for: indexPath) as! ReboundListTableViewCell
+        
+        return cell
+    }
+}
